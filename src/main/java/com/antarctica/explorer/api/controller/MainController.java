aef5b6d3bef151a8ce46716642cf24bcf9d5ef93
Danielle Osazuwa <dsazuwa@gmail.com>
2024-01-18 20:33:34 +0100

    feat: create base route for getting cruiselines and expeditions

---
+++ src/main/java/com/antarctica/explorer/api/controller/MainController.java
@@ -0,0 +1,34 @@
+package com.antarctica.explorer.api.controller;
+
+import com.antarctica.explorer.api.dto.ExpeditionDTO;
+import com.antarctica.explorer.api.model.CruiseLine;
+import com.antarctica.explorer.api.service.CruiseLineService;
+import com.antarctica.explorer.api.service.ExpeditionService;
+import java.util.List;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+  private final CruiseLineService cruiseLineService;
+  private final ExpeditionService expeditionService;
+
+  @Autowired
+  public MainController(CruiseLineService cruiseLineService, ExpeditionService expeditionService) {
+    this.cruiseLineService = cruiseLineService;
+    this.expeditionService = expeditionService;
+  }
+
+  @GetMapping("/api")
+  public MainResponse getData() {
+    Map<String, CruiseLine> cruiseLines = cruiseLineService.getCruiseLines();
+    List<ExpeditionDTO> expeditions = expeditionService.findAll();
+
+    return new MainResponse(cruiseLines, expeditions);
+  }
+
+  public record MainResponse(
+      Map<String, CruiseLine> cruiseLines, List<ExpeditionDTO> expeditions) {}
+}
